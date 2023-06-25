46eaef7a669e3c92cae1b913c01688f326b459e3
羞涩的小正太 <13078202+wuzhangda@user.noreply.gitee.com>
2023-06-25 15:26:54 +0800

    reggie第六天内容提交 查看订单{功能完成}

--- src/main/java/com/bbu/reggie/controller/OrdersController.java
+++ src/main/java/com/bbu/reggie/controller/OrdersController.java
@@ -1,14 +1,14 @@
 package com.bbu.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bbu.reggie.common.BaseContext;
 import com.bbu.reggie.common.R;
 import com.bbu.reggie.entity.Orders;
 import com.bbu.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -21,5 +21,18 @@ public class OrdersController {
 //log.info("AddressBookId={}",orders.getAddressBookId());
         ordersService.submit(orders);
         return R.success("success");
+    }
+
+    @GetMapping("/userPage")
+    public R<Page> userPage(int page, int pageSize){
+        Page<Orders> PageInfo = new Page<>(page,pageSize);
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Orders::getUserId, BaseContext.getCurrentId());
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+
+        Page<Orders> ordersPage = ordersService.page(PageInfo, queryWrapper);
+
+
+        return R.success(ordersPage);
     }
 }
